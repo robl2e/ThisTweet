22cca44e050586b116b9ce75c5bf280549107d6d
robl2e <robleewc@gmail.com>
2017-10-01 21:51:24 -0700

    Fix Medium size object.

--- app/src/main/java/com/robl2e/thistweet/data/model/timeline/Medium.java
+++ app/src/main/java/com/robl2e/thistweet/data/model/timeline/Medium.java
@@ -1,165 +1,43 @@
 
 package com.robl2e.thistweet.data.model.timeline;
 
-import java.util.List;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 public class Medium {
 
-    @SerializedName("id")
+    @SerializedName("w")
     @Expose
-    private Integer id;
-    @SerializedName("id_str")
+    private Integer w;
+    @SerializedName("h")
     @Expose
-    private String idStr;
-    @SerializedName("indices")
+    private Integer h;
+    @SerializedName("resize")
     @Expose
-    private List<Integer> indices = null;
-    @SerializedName("media_url")
-    @Expose
-    private String mediaUrl;
-    @SerializedName("media_url_https")
-    @Expose
-    private String mediaUrlHttps;
-    @SerializedName("url")
-    @Expose
-    private String url;
-    @SerializedName("display_url")
-    @Expose
-    private String displayUrl;
-    @SerializedName("expanded_url")
-    @Expose
-    private String expandedUrl;
-    @SerializedName("type")
-    @Expose
-    private String type;
-    @SerializedName("sizes")
-    @Expose
-    private Sizes sizes;
-    @SerializedName("source_status_id")
-    @Expose
-    private Integer sourceStatusId;
-    @SerializedName("source_status_id_str")
-    @Expose
-    private String sourceStatusIdStr;
-    @SerializedName("source_user_id")
-    @Expose
-    private Integer sourceUserId;
-    @SerializedName("source_user_id_str")
-    @Expose
-    private String sourceUserIdStr;
+    private String resize;
 
-    public Integer getId() {
-        return id;
+    public Integer getW() {
+        return w;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
+    public void setW(Integer w) {
+        this.w = w;
     }
 
-    public String getIdStr() {
-        return idStr;
+    public Integer getH() {
+        return h;
     }
 
-    public void setIdStr(String idStr) {
-        this.idStr = idStr;
+    public void setH(Integer h) {
+        this.h = h;
     }
 
-    public List<Integer> getIndices() {
-        return indices;
+    public String getResize() {
+        return resize;
     }
 
-    public void setIndices(List<Integer> indices) {
-        this.indices = indices;
-    }
-
-    public String getMediaUrl() {
-        return mediaUrl;
-    }
-
-    public void setMediaUrl(String mediaUrl) {
-        this.mediaUrl = mediaUrl;
-    }
-
-    public String getMediaUrlHttps() {
-        return mediaUrlHttps;
-    }
-
-    public void setMediaUrlHttps(String mediaUrlHttps) {
-        this.mediaUrlHttps = mediaUrlHttps;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getDisplayUrl() {
-        return displayUrl;
-    }
-
-    public void setDisplayUrl(String displayUrl) {
-        this.displayUrl = displayUrl;
-    }
-
-    public String getExpandedUrl() {
-        return expandedUrl;
-    }
-
-    public void setExpandedUrl(String expandedUrl) {
-        this.expandedUrl = expandedUrl;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public Sizes getSizes() {
-        return sizes;
-    }
-
-    public void setSizes(Sizes sizes) {
-        this.sizes = sizes;
-    }
-
-    public Integer getSourceStatusId() {
-        return sourceStatusId;
-    }
-
-    public void setSourceStatusId(Integer sourceStatusId) {
-        this.sourceStatusId = sourceStatusId;
-    }
-
-    public String getSourceStatusIdStr() {
-        return sourceStatusIdStr;
-    }
-
-    public void setSourceStatusIdStr(String sourceStatusIdStr) {
-        this.sourceStatusIdStr = sourceStatusIdStr;
-    }
-
-    public Integer getSourceUserId() {
-        return sourceUserId;
-    }
-
-    public void setSourceUserId(Integer sourceUserId) {
-        this.sourceUserId = sourceUserId;
-    }
-
-    public String getSourceUserIdStr() {
-        return sourceUserIdStr;
-    }
-
-    public void setSourceUserIdStr(String sourceUserIdStr) {
-        this.sourceUserIdStr = sourceUserIdStr;
+    public void setResize(String resize) {
+        this.resize = resize;
     }
 
 }
